2fbe78f3a964059f47dc11d32d6750c6ddceb4eb
Matheus Castro <m.s.castro@hotmail.com>
2025-02-02 16:46:37 -0300

    Assignment 3 part 1 completed

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,9 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,6 +21,11 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int ret;
+    ret = system(cmd);
+    if (ret == -1) {
+        return false;
+    }
 
     return true;
 }
@@ -47,7 +57,7 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    // command[count] = command[count];
 
 /*
  * TODO:
@@ -58,8 +68,31 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
-
     va_end(args);
+
+    pid_t pid;
+    int status;
+
+    if (command[0][0] != '/') {
+        return false;
+    }
+
+    pid = fork();
+    if (pid == -1) {
+        return false;
+    } else if (pid == 0) {
+        execv(command[0], &command[0]);
+        perror("execv failed");
+        _exit(EXIT_FAILURE);
+    }
+
+    if (waitpid(pid, &status, 0) == -1) {
+        return false;
+    } else if (WIFEXITED(status)) {
+        if (WEXITSTATUS(status) != 0) {
+            return false;
+        }
+    }
 
     return true;
 }
@@ -82,7 +115,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    // command[count] = command[count];
 
 
 /*
@@ -92,8 +125,46 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
-
     va_end(args);
+
+    pid_t pid;
+    int status;
+
+    if (command[0][0] != '/') {
+        return false;
+    }
+
+    pid = fork();
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) {
+        perror("open");
+        abort();
+    }
+    switch (pid = fork()) {
+        case -1:
+            perror("fork");
+            return false;
+        case 0:
+            if (dup2(fd, 1) < 0) {
+                perror("dup2");
+                return false;
+            }
+            close(fd);
+            execv(command[0], &command[0]);
+            perror("execv failed");
+            _exit(EXIT_FAILURE);
+            return false;
+        default:
+            close(fd);
+    }
+
+    if (waitpid(pid, &status, 0) == -1) {
+        return false;
+    } else if (WIFEXITED(status)) {
+        if (WEXITSTATUS(status) != 0) {
+            return false;
+        }
+    }
 
     return true;
 }
