542551d0bd87e8d2ab3db4e81ddc8f5400b964ba
Matheus Castro <m.s.castro@hotmail.com>
2025-02-09 11:11:28 -0300

    Assignment 3 - part 2 finished

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,9 +35,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} all
+    # make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +54,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd "$OUTDIR"/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +67,69 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
+cd ${OUTDIR}/rootfs
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+
+interpreter=$(${CROSS_COMPILE}readelf -l bin/busybox | grep "program interpreter" | awk -F': ' '{print $2}' | tr -d '[]')
+if [ -n "$interpreter" ]; then
+    interpreter_path=$(find "$SYSROOT" -type f -path "*$interpreter")
+    if [ -n "$interpreter_path" ]; then
+        echo "Copiando o program interpreter $interpreter de $interpreter_path para "$OUTDIR"/rootfs/lib"
+        cp "$interpreter_path" "$OUTDIR"/rootfs/lib
+    else
+        echo "Program interpreter $interpreter não encontrado no sysroot."
+    fi
+else
+    echo "Nenhum program interpreter encontrado no binário."
+fi
+
+libs=$(${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | awk -F'[][]' '{print $2}')
+for lib in $libs; do
+    lib_path=$(find "$SYSROOT" -type f -name "$lib")
+    if [ -n "$lib_path" ]; then
+        echo "Copiando $lib de $lib_path"
+        cp "$lib_path" "$OUTDIR"/rootfs/lib64
+    else
+        echo "Biblioteca $lib não encontrada no sysroot"
+    fi
+done
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/nulll c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd "$FINDER_APP_DIR"
+make CROSS_COMPILE=${CROSS_COMPILE} clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp finder-test.sh finder.sh writer autorun-qemu.sh "$OUTDIR"/rootfs/home
+mkdir "$OUTDIR"/rootfs/home/conf
+cp ../conf/username.txt ../conf/assignment.txt "$OUTDIR"/rootfs/home/conf
+sed -i 's|^assignment=`cat ../conf/assignment.txt`$|assignment=`cat conf/assignment.txt`|' "$OUTDIR"/rootfs/home/finder-test.sh
 
 # TODO: Chown the root directory
+cd "$OUTDIR"/rootfs
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > "$OUTDIR"/initramfs.cpio
+cd "$OUTDIR"
+gzip -f initramfs.cpio
